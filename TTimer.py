18cddb3aad5a55fc40b201475317723ba9047de3
Kuhschubser <loosechristian@aol.com>
2019-09-02 18:41:25 +0200

    Timing the Boi next Door!

---
+++ TTimer.py
@@ -0,0 +1,15 @@
+import time
+from _thread import *
+
+
+class TTimer:
+
+    def __init__(self):
+        self.timers = []
+
+    def set_timer(self, index, period):
+        self.timers[index] = start_new_thread(self.threaded_timer(period, index), ())
+
+    def threaded_timer(self, period, index):
+        time.sleep(period)
+        self.timers[index] = 0
