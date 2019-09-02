b6b0f3a4d51559820927d91836d8ecdf968f52b2
Kuhschubser <loosechristian@aol.com>
2019-09-02 21:01:27 +0200

    Timing the Boi next Door!

--- TTimer.py
+++ TTimer.py
@@ -8,8 +8,13 @@ class TTimer:
         self.timers = []
 
     def set_timer(self, index, period):
-        self.timers[index] = start_new_thread(self.threaded_timer(period, index), ())
+        self.timers[index] = start_new_thread(self.timer_function_binder(period, index), ())
 
-    def threaded_timer(self, period, index):
-        time.sleep(period)
-        self.timers[index] = 0
+    def timer_function_binder(self, period, index):
+
+        def threaded_timer():
+            time.sleep(period)
+            self.timers[index] = 0
+
+        threaded_timer().__name__ = "lel"
+        return threaded_timer()
