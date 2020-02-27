490759bf5b3fc68cbe9f67844e7ad69b810f783b
Kuhschubser <loosechristian@aol.com>
2020-02-27 14:51:11 +0100

    Networking the Networking the Render in recursive

--- Render.py
+++ Render.py
@@ -122,6 +122,8 @@ class ConnectionSetup:
         self.screen = None
         self.buttons = None
 
+        self.hosting_list = None
+
         self.ip_field_text = "No games available"  # TODO change; this will be a button in a list of games to join marking the game as the game to join
         self.desi_board_text = "50"#"Enter board size"  # TODO change; here you should also enter the game name -> GameName, size, e.g. Dungeon, 50
         self.game_map_string = None
@@ -140,31 +142,36 @@ class ConnectionSetup:
 
         size = true_res
 
+        if self.hosting_list:
+            # TODO this is hardcoded; always choosing game 1 from hosting list
+            self.game_to_join = self.hosting_list["Dungeon"]
+            self.ip_field_text = "{}, {} Points".format(self.game_to_join.name, self.game_to_join.points)
+
+            ''' At a later point, the ip_to_join_button should be reworked as a list, containing 1 button per 
+            game in the hosting list. If you click the button, the corresponding game should be game_to_join. 
+            Clicking the join button should do the joining then'''
+        else:
+            self.game_to_join = None
+            self.ip_field_text = "No games available"
+
         # Asking for tha hosting list all 3 seconds assuming 60 FPS
 
         # this number has to be big enough to receive the list meanwhile
         if self.get_hosting_list_counter >= 60:  # TODO maybe lower this number to 1s?
-            hosting_list = self.client.get_hosting_list()
+            self.hosting_list = self.client.get_hosting_list()
+            print("*"*30)
+            print(self.hosting_list)
 
             print("-"*30 + "\nRec log len:", self.client.connection.get_rec_log_len())
             for elem in self.client.connection.get_rec_log()[-10:]:
                 print("\n", elem.to_string())
             print()
 
-            if hosting_list:
-                # TODO this is hardcoded; always choosing game 1 from hosting list
-                self.game_to_join = hosting_list["Dungeon"]
-                self.ip_field_text = "{}, {} Points".format(self.game_to_join.name, self.game_to_join.points)
-
-                ''' At a later point, the ip_to_join_button should be reworked as a list, containing 1 button per 
-                game in the hosting list. If you click the button, the corresponding game should be game_to_join. 
-                Clicking the join button should do the joining then'''
-            else:
-                self.game_to_join = None
-                self.ip_field_text = "No games available"
             self.get_hosting_list_counter = 0
         else:
             self.get_hosting_list_counter += 1
+
+
 
         # set up GUI ---------------------------------------------------------------------------------------------------
 
