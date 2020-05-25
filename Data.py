d89c057364db8c243896f453901459c976b60c8f
python-in-my-pants <sora-no-isan@gmx.de>
2020-05-25 20:21:43 +0200

    commiteee

--- Data.py
+++ Data.py
@@ -27,13 +27,13 @@ def points_to_spend_per_team(x, y):
     return int(x * y * points_per_field * 0.5)
 
 
-#serverIP = "88.130.48.19"
+#serverIP = "88.130.48.88"
 
 
-serverIP = "78.47.178.105"
+#serverIP = "78.47.178.105"
 
 
-#serverIP = "localhost"
+serverIP = "localhost"
 
 
 #serverIP = "25.68.211.167"
